c8fd679436d48187d65d7da2a939cbe53c52c38e
Yasuharu Sawada <yasuharu519@gmail.com>
2014-08-25 02:28:44 +0900

    Updates

---
+++ index.js
@@ -0,0 +1,43 @@
+(function() {
+  var plot;
+
+  plot = function(yasuharu, elm, title, ysample, key, line) {
+    return $(elm).highcharts({
+      colors: [line],
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'datetime',
+        title: {
+          text: 'Date'
+        }
+      },
+      yAxis: {
+        title: {
+          text: ysample
+        }
+      },
+      series: [
+        {
+          name: title,
+          data: _.map(yasuharu.data, function(row) {
+            var date;
+            date = row.datetime.split(' ')[0].split('/');
+            return [Date.UTC(date[0], date[1] - 1, date[2]), row[key]];
+          })
+        }
+      ]
+    });
+  };
+
+  $.getJSON('https://rawgit.com/yasuharu519/Weight/master/data.json').done(function(yasuharu) {
+    plot(yasuharu, '#weight', 'Weight', 'Weight (kg)', 'weight', '#7cb5ec');
+    plot(yasuharu, '#fat', 'Fat', '(%)', 'fat', '#827eef');
+    return plot(yasuharu, '#bmi', 'BMI', '(score)', 'bmi', '#d180f2');
+  });
+
+}).call(this);
