de13b7f1dbfa23dd13fe70a608c34bc60d8ce03b
Yasuharu Sawada <yasuharu519@gmail.com>
2020-12-20 23:44:30 +0900

    coffee script を削除

---
+++ graph/Gruntfile.js
@@ -0,0 +1,92 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pug: {
+      compile: {
+        files: {
+          'build/index.html': ['index.pug']
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 5000,
+          base: 'build/',
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      pages: {
+        files: ['index.pug'],
+        tasks: ['pug']
+      },
+      scripts: {
+        files: ['index.coffee'],
+        tasks: ['coffee']
+      },
+      styles: {
+        files: ['index.styl'],
+        tasks: ['stylus']
+      },
+      build: {
+        files: ['build/**'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    open: {
+      build: {
+        path: 'http://localhost:5000'
+      }
+    },
+
+    coffee: {
+      main: {
+        files: {
+          'build/index.js': ['index.coffee']
+        }
+      }
+    },
+
+    stylus: {
+      style: {
+        files: {
+          'build/index.css': ['index.styl']
+        }
+      }
+    },
+
+    clean: {
+      debuild: ['build/', '.grunt/']
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'build/'
+      },
+      src: ['**']
+    }});
+
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['pug', 'coffee', 'stylus', 'connect', 'open', 'watch']);
+  grunt.registerTask('deploy',  ['clean', 'pug', 'coffee', 'stylus', 'gh-pages']);
+  return grunt.registerTask('build',  ['clean', 'pug', 'coffee', 'stylus']);
+};
---
+++ graph/index.js
@@ -0,0 +1,41 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const plot = (yasuharu, elm, title, ysample, key, line) => $(elm).highcharts({
+  colors: [line],
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: title
+  },
+  xAxis: {
+    type: 'datetime',
+    title: {
+      text: 'Date'
+    }
+  },
+  yAxis: {
+    title: {
+      text: ysample
+    }
+  },
+  series: [{
+    name: title,
+    data: _.map(yasuharu.data, function(row){
+      const date = row.datetime.split(' ')[0].split('/');
+      return [
+        Date.UTC(date[0], (date[1]-1), date[2]),
+        row[key]
+      ];
+  })
+  }
+  ]});
+
+$.getJSON('https://raw.githubusercontent.com/yasuharu519/Weight/master/data.json').done(function(yasuharu){
+  plot(yasuharu, '#weight', 'Weight', 'Weight (kg)', 'weight', '#7cb5ec');
+  plot(yasuharu, '#fat',    'Fat',    '(%)',         'fat',    '#827eef');
+  return plot(yasuharu, '#bmi',    'BMI',    '(score)',     'bmi',    '#d180f2');
+});
