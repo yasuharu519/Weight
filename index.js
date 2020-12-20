c29c40854d21640ccb1c52745727498d567b29de
yasuharu519 <yasuharu519@users.noreply.github.com>
2020-12-20 15:16:59 +0000

    deploy: a9262a0713f8ac5d317d06d3b6ad8a24127001a4

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-(function() {
-  var plot;
+"use strict";
 
-  plot = function(yasuharu, elm, title, ysample, key, line) {
-    return $(elm).highcharts({
-      colors: [line],
-      chart: {
-        type: 'spline'
-      },
+var plot = function plot(yasuharu, elm, title, ysample, key, line) {
+  return $(elm).highcharts({
+    colors: [line],
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: title
+    },
+    xAxis: {
+      type: 'datetime',
       title: {
-        text: title
-      },
-      xAxis: {
-        type: 'datetime',
-        title: {
-          text: 'Date'
-        }
-      },
-      yAxis: {
-        title: {
-          text: ysample
-        }
-      },
-      series: [
-        {
-          name: title,
-          data: _.map(yasuharu.data,
-        function(row) {
-            var date;
-            date = row.datetime.split(' ')[0].split('/');
-            return [Date.UTC(date[0],
-        date[1] - 1,
-        date[2]),
-        row[key]];
-          })
-        }
-      ]
-    });
-  };
-
-  $.getJSON('https://raw.githubusercontent.com/yasuharu519/Weight/master/data.json').done(function(yasuharu) {
-    plot(yasuharu, '#weight', 'Weight', 'Weight (kg)', 'weight', '#7cb5ec');
-    plot(yasuharu, '#fat', 'Fat', '(%)', 'fat', '#827eef');
-    return plot(yasuharu, '#bmi', 'BMI', '(score)', 'bmi', '#d180f2');
+        text: 'Date'
+      }
+    },
+    yAxis: {
+      title: {
+        text: ysample
+      }
+    },
+    series: [{
+      name: title,
+      data: _.map(yasuharu.data, function (row) {
+        var date = row.datetime.split(' ')[0].split('/');
+        return [Date.UTC(date[0], date[1] - 1, date[2]), row[key]];
+      })
+    }]
   });
+};
 
-}).call(this);
+$.getJSON('https://raw.githubusercontent.com/yasuharu519/Weight/master/data.json').done(function (yasuharu) {
+  plot(yasuharu, '#weight', 'Weight', 'Weight (kg)', 'weight', '#7cb5ec');
+  plot(yasuharu, '#fat', 'Fat', '(%)', 'fat', '#827eef');
+  plot(yasuharu, '#bmi', 'BMI', '(score)', 'bmi', '#d180f2');
+});
+//# sourceMappingURL=index.js.map
